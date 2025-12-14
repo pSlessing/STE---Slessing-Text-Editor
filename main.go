c0b810047f2a2879e44e4fa5329544bff9f2f114
Peter Slessing <peter@slessing.dk>
2025-12-14 12:57:43 +0100

    formatting fixed i hope

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-// main.go //Blabla
 package main
 
 import (
@@ -13,8 +12,5 @@ func main() {
 		fmt.Printf("Failed to initialize editor: %v\n", err)
 		os.Exit(1)
 	}
-
-	// Plugins are auto-loaded in Run()
-	// Wow, i can comment in here
 	editor.Run()
 }
